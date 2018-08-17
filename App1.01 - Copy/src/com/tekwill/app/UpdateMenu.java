7011b0962d4c3e9c9e4cb6a84e030d7e43e6668c
Andrei <cerneleaaa@gmail.com)>
2018-08-17 17:30:57 +0300

    Updated Version 1.01

--- App1.01 - Copy/src/com/tekwill/app/UpdateMenu.java
+++ App1.01 - Copy/src/com/tekwill/app/UpdateMenu.java
@@ -87,7 +87,7 @@ public class UpdateMenu extends javax.swing.JFrame {
 
         jLabel1.setText("Enter Name");
 
-        jButton1.setText("jButton1");
+        jButton1.setText("Update");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
